694001bf6ad17b1359229e463dae6872a36e6704
MariiaNk <mariya.nyzhnyk2006@gmail.com>
2024-02-29 19:11:53 +0200

    Add merge sort, combine sort algorithms. Add benchmark mode pdf

--- Semestr 2/Lab 3a - Sorting algo.cpp
+++ Semestr 2/Lab 3a - Sorting algo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <chrono>
 const int NMAX = 1e3;
 
@@ -79,6 +80,72 @@ void quickSort(string *arr, int st, int fn)
         quickSort(arr, middle + 1, fn);
     }
 }
+
+void merge(string *arr, int st, int mid, int fn)
+{
+    int i, j, k;
+    int n1 = mid - st + 1;
+    int n2 = fn - mid;
+
+    string L[n1], R[n2]; 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[st + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1+ j];
+    i = j = 0;
+    k = st;
+    while (i < n1 && j < n2)
+    {
+        if (R[j] >= L[i])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(string *arr, int n)
+{ 
+    for (int i = 1; i <= n - 1; i *= 2) // current Size
+        for (int j = 0; j < n-1; j += 2*i) // start of sub array
+        {
+            int middle = min(j + i - 1, n-1);
+            int rEnd = min(j + 2*i - 1, n-1);
+            merge(arr, j, middle, rEnd);
+        }
+}
+
+void combineSort(string *arr, int n)
+{
+    if(n > 100) 
+    {
+        //cout << "Big..." << endl;
+        quickSort(arr, 0, n - 1);
+    }
+    else 
+    {
+        //cout << "Small..." << endl;
+        insertionSort(arr, n);
+    }
+}
 void outputArr(const string *arr, int n)
 {
     cout << "==== ARRAY ====" << endl;
@@ -100,6 +167,10 @@ string GenerateString()
     return str;
 }
 
+bool cmp(string a, string b)
+{
+    return b > a;
+}
 
 void DemonstrationMode()
 {
@@ -109,16 +180,40 @@ void DemonstrationMode()
 
     copyArray(copy, arr, n);
     outputArr(copy, n);
-    cout << "***  INSERTION SORT  ***\n [Process is going...]\n";
+    cout << "***  INSERTION SORT  ***\n";
     insertionSort(copy, n);
     outputArr(copy, n);
+    cout << endl;
 
     copyArray(copy, arr, n);
     outputArr(copy, n);
-    cout << "***  QUICK SORT  ***\n [Process is going...]\n";
+    cout << "***  QUICK SORT  ***\n";
     quickSort(copy, 0, n - 1);
     outputArr(copy, n);
+    cout << endl;
+
+    copyArray(copy, arr, n);
+    outputArr(copy, n);
+    cout << "***  MERGE SORT  ***\n";
+    mergeSort(copy, n);
+    outputArr(copy, n);
+    cout << endl;
+
+    copyArray(copy, arr, n);
+    outputArr(copy, n);
+    cout << "***  COMBINE SORT [QUICK/INSERTION]  ***\n";
+    combineSort(copy, n);
+    outputArr(copy, n);
+    cout << endl;
+
+    copyArray(copy, arr, n);
+    outputArr(copy, n);
+    cout << "***  STANDART SORT  ***\n";
+    sort(copy,  copy + n, cmp);
+    outputArr(copy, n);
+    cout << endl;
 }
+
 void BenchmarkMode()
 {
     string arr[NMAX], copy[NMAX];
@@ -133,15 +228,36 @@ void BenchmarkMode()
     auto start = high_resolution_clock::now();
     insertionSort(copy, n);
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(stop - start);
-    cout << "Time taken by INSERTION SORT:  " << duration.count() << " milliseconds" << endl;
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by INSERTION SORT:  " << duration.count() << " microseconds" << endl;
 
     copyArray(copy, arr, n);
     start = high_resolution_clock::now();
     quickSort(copy, 0, n - 1);
     stop = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(stop - start);
-    cout << "Time taken by QUICK SORT:  " << duration.count() << " milliseconds" << endl;
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by QUICK SORT:  " << duration.count() << " microseconds" << endl;
+
+    copyArray(copy, arr, n);
+    start = high_resolution_clock::now();
+    mergeSort(copy, n);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by MERGE SORT:  " << duration.count() << " microseconds" << endl;
+
+    copyArray(copy, arr, n);
+    start = high_resolution_clock::now();
+    combineSort(copy, n);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by COMBINE SORT:  " << duration.count() << " microseconds" << endl;
+
+    copyArray(copy, arr, n);
+    start = high_resolution_clock::now();
+    sort(arr, arr+n, cmp);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by STANDART SORT:  " << duration.count() << " microseconds" << endl;
     
 
 }
