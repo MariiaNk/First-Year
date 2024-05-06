68fee33a49a7e609a867435d0b9882958e2a12aa
MariiaNk <mariya.nyzhnyk2006@gmail.com>
2024-05-06 11:27:15 +0300

    Correct function add (type of item), add function `search by range` and function `del`

--- Semestr 2/Lab 6 - Ordered list.cpp
+++ Semestr 2/Lab 6 - Ordered list.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
@@ -48,6 +49,14 @@ struct complex
             return compare(other);
         return thisMod > otherMod;
     }
+    bool operator < (const complex& other) const
+    {
+        int thisMod = this->funcCompare();
+        int otherMod = other.funcCompare();
+        if (thisMod != otherMod)
+            return !(compare(other));
+        return thisMod < otherMod;
+    }
     bool operator == (const complex& other) const
     {
         return (real == other.real) && (imaginary == other.imaginary);
@@ -65,7 +74,7 @@ struct Node
 {
     complex value;
     Node* next;
-    Node(int valueItem = 0)
+    Node(complex valueItem = 0)
     {
         value = valueItem;
         next = nullptr;
@@ -79,7 +88,7 @@ struct orderedLinkedList
     {
         head = nullptr;
     }
-    void add(int item) 
+    void add(complex item) 
     {
         if (head == nullptr) 
         {
@@ -133,6 +142,36 @@ struct orderedLinkedList
         return found;
     }
 
+    void del(complex item) 
+    {
+        Node *curr = head, *prev = nullptr;
+        while (curr != nullptr && !(curr->value == item)) 
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        if (curr == nullptr) return;
+
+        if (prev == nullptr) 
+            head = curr->next; 
+        else
+            prev->next = curr->next;
+        delete curr;
+    }
+
+    vector<complex> searchByRange(complex minValue, complex maxValue) 
+    {
+        vector<complex> results;
+        Node* curr = head;
+        while (curr != nullptr && curr->value < minValue) 
+            curr = curr->next; 
+        while (curr != nullptr && (curr->value < maxValue || curr->value == maxValue)) 
+        {
+            results.push_back(curr->value);
+            curr = curr->next;
+        }
+        return results;
+    }
     void print()
     {
         Node *current = head;
@@ -147,16 +186,22 @@ struct orderedLinkedList
 int main()
 {
     orderedLinkedList myList;
-    myList.add(15);
-    myList.add(45);
-    myList.add(20);
+    myList.add({15, 4});
+    myList.add({45, 1});
+    myList.add({34, 0});
     myList.add(56);
     myList.add(1);
     myList.print();
-    if(myList.search(15))
+    if(myList.search({15, 4}))
         cout << "Yes\n";
     else cout << "No\n";
     if(myList.search(19))
         cout << "Yes\n";
     else cout << "No\n";
+    myList.del({56, 0});
+    vector <complex> searchResult = myList.searchByRange({13, 4}, {56, 8});
+    for(int i = 0; i < searchResult.size(); i++)
+    {
+        cout << searchResult[i] << "; ";
+    }
 }
