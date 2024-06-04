df9a88aa3de1f103c382b1c778784e37d5988e3d
MariiaNk <mariya.nyzhnyk2006@gmail.com>
2024-06-04 12:23:22 +0300

    Add task with fixed distance

--- Summer Exam/Graph1.cpp
+++ Summer Exam/Graph1.cpp
@@ -1,7 +1,5 @@
-/*Написати функцію, яка перевіряє суміжність двох заданих вершин 
-у неорієнтованому графі, що поданий структурою суміжності */
-
 #include <iostream>
+#include <queue>
 #define NMAX 10000
 
 using namespace std;
@@ -26,6 +24,8 @@ struct structAdj
     }
 };
 
+/*Написати функцію, яка перевіряє суміжність двох заданих вершин 
+у неорієнтованому графі, що поданий структурою суміжності */
 bool checkAdjacency(structAdj graph, int st, int fn)
 {
     if(st < 0 || st >= graph.n || fn < 0 || fn > graph.n)
@@ -41,9 +41,46 @@ bool checkAdjacency(structAdj graph, int st, int fn)
     return false;
 }
 
+//Алгоритм (Дейкстри) - Обчислення відстаней від заданої вершини
+int* distance_from_vertex(structAdj graph, int start)
+{
+    int* distance = new int[NMAX];
+    for(int i = 0; i < NMAX; i++)
+        distance[i] = INT_MAX;
+    
+    distance[start] = 0;
+    queue <int> que;
+    que.push(start);
+    while(!que.empty())
+    {
+        int vertex = que.front();
+        que.pop();
+        Node* current = graph.neighbours[vertex];
+        while(current)
+        {
+            if(distance[current->value] == INT_MAX)
+            {
+                distance[current->value] = distance[vertex] + 1;
+                que.push(current->value);
+            }
+            current = current->next;
+        }
+    }
+    return distance;
+}
+
+void find_vertex_fixed_distance(structAdj graph, int vertex, int distance)
+{
+    int* dist = distance_from_vertex(graph, vertex);
+    for(int i = 0 ; i < graph.n; i++)
+    {
+        if(dist[i] == distance)
+            cout << i << " ";
+    }
+}
 void add(structAdj &graph, int st, int fn)
 {
-    graph.n = max(graph.n, max(st, fn));
+    graph.n = max(graph.n, max(st + 1, fn + 1));
     if(!graph.neighbours[st])
         graph.neighbours[st] = new Node(fn);
     else
@@ -84,8 +121,12 @@ int main()
     structAdj graph;
     add(graph, 2, 5);
     add(graph, 1, 3);
-    cout << "+";
+    add(graph, 0, 4);
+    add(graph, 1, 4);
+    add(graph, 5, 4);
     print(graph);
-    if(checkAdjacency(graph, 1, 2)) cout << "YES\n";
-    else cout<<"NO\n";
+    /*if(checkAdjacency(graph, 1, 2)) cout << "YES\n";
+    else cout<<"NO\n";*/
+
+    find_vertex_fixed_distance(graph, 3, 3);
 }
